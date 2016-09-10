b56abc76cb3d9ab2b1a94391097153a1ed629a08
Sindre Vingen <sindre@debian>
2016-09-10 16:56:27 +0200

    Implementerte SHA og MD5-hashing, samt lagde tester for disse.

--- examples.cpp
+++ examples.cpp
@@ -12,5 +12,14 @@ int main() {
   cout << Crypto::hex(Crypto::sha1("Test", 2)) << endl << endl;
 
   cout << "The derived key from the PBKDF2 algorithm" << endl;
-  cout << Crypto::hex(Crypto::pbkdf2("Password", "Salt2")) << endl;
+  cout << Crypto::hex(Crypto::pbkdf2("Password", "Salt")) << endl << endl;
+
+  cout << "MD5 with 1 iteration" << endl;
+  cout << Crypto::hex(Crypto::md5("Test")) << endl << endl;
+  
+  cout << "SHA-256 with 1 iteration" << endl;
+  cout << Crypto::hex(Crypto::sha256("Test")) << endl << endl;
+   
+   cout << "SHA-512 with 1 iteration" << endl;
+   cout << Crypto::hex(Crypto::sha512("Test")) << endl << endl;
 }
